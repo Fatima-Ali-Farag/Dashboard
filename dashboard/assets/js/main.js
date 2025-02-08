d7fdaef78b1c63d6f9d30bc738a3064d12fdd68f
Fatima Ali Farag <104573060+Fatima-Ali-Farag@users.noreply.github.com>
2025-02-09 00:19:31 +0200

    Add files via upload

---
+++ dashboard/assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#article1").click(function(){
+      $("#table1").slideToggle("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#article2").click(function(){
+      $("#table2").slideToggle("slow");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#article3").click(function(){
+      $("#table3").slideToggle("slow");
+    });
+  });
